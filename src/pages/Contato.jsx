49ff6c6d4c67d8a321599aa6fe795712886aa6c6
SIMONE QUEIROZ BATISTA <simone.qbatista@senacsp.edu.br>
2025-04-29 19:28:50 -0300

    atualização arquivo contato e module.css

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -2,7 +2,46 @@ import styles from "./Contato.module.css";
 
 export function Contato() {
   return (
-    
-    <div>Contato</div>
-  )
+    <section id="contact" className={styles.contato}>
+      <div className={styles.imageContainer}>
+        <img src="images/office.jpg" alt="Advogados no escritório" />
+      </div>
+      <div className={styles.informacoesContato}>
+        <i className={`bi bi-envelope ${styles.iconeContato}`}></i>
+        <p className={styles.subtituloContato}>Entre em contato</p>
+        <form className={styles.formulario}>
+          <div className={styles.grupoCampos}>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Nome"
+              className={styles.campo}
+            />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="E-mail"
+              className={styles.campo}
+            />
+          </div>
+          <input
+            type="text"
+            name="phone"
+            id="phone"
+            placeholder="Telefone"
+            className={styles.campo}
+          />
+          <textarea
+            name="message"
+            id="message"
+            placeholder="Sua mensagem..."
+            className={styles.mensagem}
+          ></textarea>
+          <input type="submit" value="Enviar" className={styles.botao} />
+        </form>
+      </div>
+    </section>
+  );
 }
